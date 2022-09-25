b848212ff253c500f5e75198668b708f6551713a
atbee <athibet.pi@gmail.com>
2022-09-25 16:47:32 +0700

    Set up the website bucket :bee:

--- index.ts
+++ index.ts
@@ -2,8 +2,30 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("my-bucket");
+import * as fs from "fs";
+import * as mime from "mime";
 
-// Export the name of the bucket
-export const bucketName = bucket.id;
+const bucket = new aws.s3.BucketV2("bucketV2", {
+    tags: {
+        Name: "<YOUR_BUCKET_NAME>", // Fix it 🚨
+    },
+});
+
+const bucketAcl = new aws.s3.BucketAclV2("bAcl", {
+    bucket: bucket.id,
+    acl: aws.s3.PublicReadAcl,
+});
+
+const staticWebsiteDirectory = "website";
+
+fs.readdirSync(staticWebsiteDirectory).forEach((file) => {
+    const filePath = `${staticWebsiteDirectory}/${file}`;
+    const fileContent = fs.readFileSync(filePath).toString();
+
+    new aws.s3.BucketObject(file, {
+        bucket: bucket.id,
+        source: new pulumi.asset.FileAsset(filePath),
+        contentType: mime.getType(filePath) || undefined,
+        acl: aws.s3.PublicReadAcl,
+    });
+});
