b14b23f0493462becb60ba8333f761e6dc378f43
atbee <athibet.pi@gmail.com>
2022-09-25 20:05:36 +0700

    Create the Checkly monitoring :bee:

--- index.ts
+++ index.ts
@@ -4,4 +4,22 @@ import { CdnWebsite } from "./cdn-website";
 
 const website = new CdnWebsite("your-startup", {});
 
-export const url = website.url;
+export const websiteUrl = website.url;
+
+// Monitoring with Checkly
+// Demonstrates Standard Package usage
+import * as checkly from "@checkly/pulumi";
+import * as fs from "fs";
+
+new checkly.Check("index-page", {
+    activated: true,
+    frequency: 10,
+    type: "BROWSER",
+    locations: ["ap-southeast-1"],
+    script: websiteUrl.apply((url) =>
+        fs
+            .readFileSync("checkly-embed.js")
+            .toString("utf8")
+            .replace("{{websiteUrl}}", url)
+    ),
+});
